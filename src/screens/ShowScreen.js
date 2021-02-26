8cffc1820acc05d80799fedf1b18cf82dbd465d4
FigueroaR <48839893+FigueroaR@users.noreply.github.com>
2021-02-26 16:23:17 -0500

    Retrieving Single Posts

--- src/screens/ShowScreen.js
+++ src/screens/ShowScreen.js
@@ -1,10 +1,17 @@
-import React from 'react';
-
+import React, {useContext} from 'react';
 import {View, Text, StyleSheet} from 'react-native';
+import {Context} from '../../src/context/BlogContext'
 
-const ShowScreen = () => {
+const ShowScreen = ( {navigation} ) => {
+    // destrcu state with UseContext hook and importing the Context to be used here
+    const {state} = useContext(Context);
+
+    const blogPost = state.find((blogPost) => blogPost.id ===navigation.getParam('id'))
+    console.log(blogPost)
+    // with ge extra params with added to our navigation
+    //console.log(navigation.getParam('id'))
     return <View>
-        <Text>Show Screen</Text>
+        <Text>{blogPost.title}</Text>
     </View>
 }
 
