3b7d5dcf648df4acd49ee9aedd97cd55fc63becc
FigueroaR <48839893+FigueroaR@users.noreply.github.com>
2021-02-26 17:05:00 -0500

    dispalying a form

--- src/screens/CreateScreen.js
+++ src/screens/CreateScreen.js
@@ -1,15 +1,40 @@
-import React, {useContext} from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import React, {useContext, useState} from 'react';
+import {View, Text, StyleSheet, TextInput, Button} from 'react-native';
 import {Context} from '../../src/context/BlogContext'
 
 const CreateScreen = ( {navigation} ) => {
-    return <View>
-        <Text>Create</Text>
-    </View>
+    const [title, setTitle] = ('')
+    const [content, setContent] = ('')
+
+    return (
+        <View>
+            <Text style={styles.label}>Enter Title:</Text>
+            <TextInput style={styles.input} value={title} onChangeText={setTitle}/>
+            <Text style={styles.label}>Enter Content:</Text>
+            <TextInput style={styles.input} value={content} onChangeText={setContent} 
+            
+            />
+            <Button title="Add Blog Post"/>
+        </View>
+    )
     
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    input: {
+        fontSize: 18,
+        borderWidth: 1,
+        borderColor: 'black',
+        marginBottom: 10,
+        padding: 5,
+        margin: 5
+    },
+    label: {
+        fontSize: 20,
+        marginBottom: 5,
+        marginLeft: 5
+    }
+})
 
 
 export default CreateScreen;
