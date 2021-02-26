04cb75fdca4cf6f0bce04e82df435ae0f1b92655
FigueroaR <48839893+FigueroaR@users.noreply.github.com>
2021-02-26 11:25:29 -0500

    it works

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,16 +1,18 @@
 import React, { useContext } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Button } from 'react-native';
 import { FlatList } from 'react-native-gesture-handler';
 import BlogContext from '../context/BlogContext';
 
 const IndexScreen = () => {
-  const blogPosts = useContext(BlogContext);
+  // we are destructuring our state and function for BlogContext
+  const {data, addBlogPost} = useContext(BlogContext);
 
   return (
     <View>
       <Text>Index Screen</Text>
+      <Button title="Add post" onPress={addBlogPost}/>
       <FlatList 
-        data={blogPosts}
+        data={data}
         keyExtractor={(blogPost) => blogPost.tittle}  
         renderItem={({item}) => {
           return(<Text>{item.title}</Text>)
