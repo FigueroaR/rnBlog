86c8c5f75c98f1cc8077d5fe46c76614747705a8
FigueroaR <48839893+FigueroaR@users.noreply.github.com>
2021-03-01 17:11:42 -0500

    Creating posts with POST request

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -34,12 +34,14 @@ const getBlogPosts = dispatch => {
 }
 
 const addBlogPost = dispatch => {
-  return (title, content, callback) => {
-    dispatch({type: 'add_blogpost', payload: {title, content} })
-    // we navigate after POSTING 
+  return async (title, content, callback) => {
+    await jsonServer.post('/blogposts', {title, content})
+    // dispatch({type: 'add_blogpost', payload: {title, content} })
+    // // we navigate after POSTING 
     if (callback()) {
       callback()
     }
+
   }
 }
 
