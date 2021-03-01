00a421148f33b8c67a0626e009dd06bce6abe6e0
FigueroaR <48839893+FigueroaR@users.noreply.github.com>
2021-03-01 17:24:51 -0500

    deleting a post

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -58,7 +58,9 @@ const editBlogPost = dispatch => {
 };
 
 const deleteBlogPost = dispatch => {
-  return (id) => {
+  return async id => {
+    await jsonServer.delete(`/blogposts/${id}`)
+
     dispatch({type: 'delete_blogpost', payload: id})
   }
 }
