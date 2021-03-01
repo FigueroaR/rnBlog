5c36b564d8eecebac208cf667f841a62237ad519
FigueroaR <48839893+FigueroaR@users.noreply.github.com>
2021-03-01 13:52:13 -0500

    Editing ina Reducer

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,20 +1,27 @@
 import createDataContext from './createDataContext'
 
 
-const blogreducer = (state, action) => {
-  switch(action.type) {
-    case 'add_blogpost': 
-      return [...state, 
-              {id: Math.floor(Math.random() * 99999) , 
-              title: action.payload.title,
-              content: action.payload.content}]
+const blogReducer = (state, action) => {
+  switch (action.type) {
+    case 'edit_blogpost':
+      return state.map(blogPost => {
+        return blogPost.id === action.payload.id ? action.payload : blogPost;
+      });
     case 'delete_blogpost':
-      return state.filter((blogPost) => blogPost.id !== action.payload)
-
+      return state.filter(blogPost => blogPost.id !== action.payload);
+    case 'add_blogpost':
+      return [
+        ...state,
+        {
+          id: Math.floor(Math.random() * 99999),
+          title: action.payload.title,
+          content: action.payload.content
+        }
+      ];
     default:
       return state;
   }
-}
+};
 
 const addBlogPost = dispatch => {
   return (title, content, callback) => {
@@ -26,10 +33,12 @@ const addBlogPost = dispatch => {
 
 const editBlogPost = dispatch => {
   return (id, title, content) => {
-    dispatch({type: 'edit_blogpost', 
-              patyload: {id, title, content} })
-  }
-}
+    dispatch({
+      type: 'edit_blogpost',
+      payload: { id, title, content }
+    });
+  };
+};
 
 const deleteBlogPost = dispatch => {
   return (id) => {
@@ -38,6 +47,6 @@ const deleteBlogPost = dispatch => {
 }
 
 //destructuring what we receive     // pass our reducer , actions to disp, and initState
-export const {Context, Provider} = createDataContext(blogreducer, 
+export const {Context, Provider} = createDataContext(blogReducer, 
                                                     {addBlogPost, deleteBlogPost, editBlogPost}, 
                                                     [{title: 'TEST POST', content: 'CONTENT', id: 1}])
